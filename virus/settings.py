23b991299d8717989f34e306eef854b412ae60b1
lingwu <1396119095@qq.com>
2021-11-04 18:43:22 +0800

    增加了一个知乎的小爬虫，主要是在url的返回的部分还需要改进

--- virus/settings.py
+++ virus/settings.py
@@ -14,10 +14,9 @@ NEWSPIDER_MODULE = 'virus.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'virus (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
